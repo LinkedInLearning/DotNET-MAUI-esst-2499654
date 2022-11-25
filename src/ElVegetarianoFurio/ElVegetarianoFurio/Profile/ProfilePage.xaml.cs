6ce988789e46405f28c41664b5e692328adf8b3c
André Krämer <andre@andrekraemer.de>
2022-11-25 21:46:29 +0100

    04_04 Ende

--- src/ElVegetarianoFurio/ElVegetarianoFurio/Profile/ProfilePage.xaml.cs
+++ src/ElVegetarianoFurio/ElVegetarianoFurio/Profile/ProfilePage.xaml.cs
@@ -5,5 +5,6 @@ public partial class ProfilePage : ContentPage
 	public ProfilePage()
 	{
 		InitializeComponent();
+		BindingContext = new ProfileViewModel();
 	}
 }
---
+++ src/ElVegetarianoFurio/ElVegetarianoFurio/Profile/ProfileViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ElVegetarianoFurio.Profile
+{
+    [INotifyPropertyChanged]
+    public partial class ProfileViewModel 
+    {
+
+        [ObservableProperty]
+        private string _fullName;
+
+        [ObservableProperty]
+        private string _street;
+
+        [ObservableProperty]
+        private string _zip;
+
+        [ObservableProperty]
+        private string _city;
+
+        [ObservableProperty]
+        private string _phone;
+
+        [ObservableProperty]
+        private bool _isBusy;
+
+
+
+        [RelayCommand]
+        private async Task SaveAsync()
+        {
+            IsBusy = true;
+            Task.Delay(3000); // simuliertes Speichern
+            IsBusy = false;
+        }
+
+
+
+    }
+}
